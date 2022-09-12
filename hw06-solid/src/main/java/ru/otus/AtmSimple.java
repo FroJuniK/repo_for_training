4e2cbaf4ab8525e13e24711d5b796e968dbb9440
Kseniya Frolova <kbf1100@gmail.com>
2022-09-12 14:16:56 +0300

    hw07-patterns fix

--- hw06-solid/src/main/java/ru/otus/AtmSimple.java
+++ hw06-solid/src/main/java/ru/otus/AtmSimple.java
@@ -2,11 +2,9 @@ package ru.otus;
 
 import ru.otus.exception.UnableToWithdrawException;
 import ru.otus.model.Banknote;
-import ru.otus.model.Cell;
 import ru.otus.model.Denomination;
 import ru.otus.model.MoneyStorage;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class AtmSimple implements Atm {
@@ -16,54 +14,18 @@ public class AtmSimple implements Atm {
         this.moneyStorage = moneyStorage;
     }
 
-    /**
-     * Добаляет все купюры в ячейки
-     */
-    public void depositMoney(List<Banknote> money) {
-        for (Banknote banknote : money) {
-            Cell cell = moneyStorage.getCell(banknote.denomination());
-            cell.putMoney(banknote);
-        }
+    public void depositMoney(List<Banknote> money) throws UnableToWithdrawException {
+        moneyStorage.depositMoney(money);
     }
 
-    /**
-     * Возвращает список купюр к выдаче.
-     * Если запрошена неккоректная сумма или недостаточно купюр, то исключение
-     */
     public List<Banknote> withdrawMoney(int value) throws UnableToWithdrawException {
-        List<Banknote> result = withdrawalCheck(value);
-        if (result == null || result.isEmpty()) {
-            throw new UnableToWithdrawException("Unable to withdraw money");
-        } else {
-            for (Banknote banknote : result) {
-                Cell cell = moneyStorage.getCell(banknote.denomination());
-                cell.pickUpMoney();
-            }
-        }
-        return result;
-    }
-
-    /**
-     * Вспомогательный метод для получения списка купюр к выдаче
-     */
-    private List<Banknote> withdrawalCheck(int value) {
-        List<Banknote> result = new ArrayList<>();
-        if (value > 0) {
-            for (Denomination denomination : Denomination.values()) {
-                while (value - denomination.getValue() >= 0) {
-                    value -= denomination.getValue();
-                    result.add(new Banknote(denomination));
-                }
-
-            }
-        }
-        return value > 0 ? null : result;
+        return moneyStorage.withdrawMoney(value);
     }
 
     /**
      * Возвращает сумму остатка денежых средств
      */
-    public int getBalance() {
+    public int getBalance() throws UnableToWithdrawException {
         int result = 0;
         for (Denomination value : Denomination.values()) {
             result += value.getValue() * moneyStorage.getCell(value).getQuantity();
--- hw06-solid/src/main/java/ru/otus/model/MoneyStorage.java
+++ hw06-solid/src/main/java/ru/otus/model/MoneyStorage.java
@@ -1,6 +1,10 @@
 package ru.otus.model;
 
+import ru.otus.exception.UnableToWithdrawException;
+
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class MoneyStorage {
@@ -10,7 +14,55 @@ public class MoneyStorage {
         cells.put(denomination, cell);
     }
 
-    public Cell getCell(Denomination denomination) {
-        return cells.get(denomination);
+    public Cell getCell(Denomination denomination) throws UnableToWithdrawException {
+        Cell cell = cells.get(denomination);
+        if (cell == null) {
+            throw new UnableToWithdrawException("No cell with denomination " + denomination.getValue());
+        }
+        return cell;
+    }
+
+    /**
+     * Добаляет все купюры в ячейки
+     */
+    public void depositMoney(List<Banknote> money) throws UnableToWithdrawException {
+        for (Banknote banknote : money) {
+            Cell cell = getCell(banknote.denomination());
+            cell.putMoney(banknote);
+        }
+    }
+
+    /**
+     * Возвращает список купюр к выдаче.
+     * Если запрошена неккоректная сумма или недостаточно купюр, то исключение
+     */
+    public List<Banknote> withdrawMoney(int value) throws UnableToWithdrawException {
+        List<Banknote> result = withdrawalCheck(value);
+        if (result == null || result.isEmpty()) {
+            throw new UnableToWithdrawException("Unable to withdraw money");
+        } else {
+            for (Banknote banknote : result) {
+                Cell cell = getCell(banknote.denomination());
+                cell.pickUpMoney();
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Вспомогательный метод для получения списка купюр к выдаче
+     */
+    private List<Banknote> withdrawalCheck(int value) {
+        List<Banknote> result = new ArrayList<>();
+        if (value > 0) {
+            for (Denomination denomination : Denomination.values()) {
+                while (value - denomination.getValue() >= 0) {
+                    value -= denomination.getValue();
+                    result.add(new Banknote(denomination));
+                }
+
+            }
+        }
+        return value > 0 ? null : result;
     }
 }
