b7ca73d74de128ea224d30d60d08ebc29e00c341
FroJuniK <kbf1100@gmail.com>
2022-09-17 11:56:13 +0300

    hw09-jdbc upd implementations

--- hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntityClassMetaDataImpl.java
@@ -1,32 +1,50 @@
 package ru.otus.jdbc.mapper;
 
+import ru.otus.annotation.Id;
+
 import java.lang.reflect.Constructor;
 import java.lang.reflect.Field;
+import java.util.Arrays;
 import java.util.List;
 
-public class EntityClassMetaDataImpl implements EntityClassMetaData {
-    @Override
-    public String getName() {
-        return null;
+public class EntityClassMetaDataImpl<T> implements EntityClassMetaData<T> {
+
+    private final Class<T> clazz;
+
+    public EntityClassMetaDataImpl(Class<T> clazz) {
+        this.clazz = clazz;
     }
 
     @Override
-    public Constructor getConstructor() {
+    public String getName() {
+        return clazz.getSimpleName();
+    }
+
+    @Override
+    public Constructor<T> getConstructor() {
+        List<Field> allFields = getAllFields();
+        for (var constructor : clazz.getConstructors()) {
+            if (constructor.getParameters().length == allFields.size()) {
+                return (Constructor<T>) constructor;
+            }
+        }
         return null;
     }
 
     @Override
     public Field getIdField() {
-        return null;
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Id.class)).findFirst().get();
     }
 
     @Override
     public List<Field> getAllFields() {
-        return null;
+        return Arrays.stream(clazz.getDeclaredFields()).toList();
     }
 
     @Override
     public List<Field> getFieldsWithoutId() {
-        return null;
+        return Arrays.stream(clazz.getDeclaredFields())
+                .filter(field -> !field.isAnnotationPresent(Id.class)).toList();
     }
 }
--- hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataImpl.java
+++ hw09-jdbc/src/main/java/ru/otus/jdbc/mapper/EntitySQLMetaDataImpl.java
@@ -1,9 +1,16 @@
 package ru.otus.jdbc.mapper;
 
 public class EntitySQLMetaDataImpl implements  EntitySQLMetaData{
+
+    private final EntityClassMetaData metaData;
+
+    public EntitySQLMetaDataImpl(EntityClassMetaData metaData) {
+        this.metaData = metaData;
+    }
+
     @Override
     public String getSelectAllSql() {
-        return null;
+        return String.format("SELECT * FROM %s", metaData.getName());
     }
 
     @Override
